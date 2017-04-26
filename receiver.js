0ab09f680002cc5f49389afa0b753d835c45d9b2
Be <bebirchall@gmail.com>
2017-04-26 13:25:26 -0400

    Add little tutorial type notes and explanations.

--- receiver.js
+++ receiver.js
@@ -1,7 +1,17 @@
 var receiverConnection;
 var receiveChannel;
 
-function start(offerString) {
+var WELCOME_MSG = 'Welcome! When you get an OFFER_STRING from a sender, copy and paste it wrapped with init, below, like this: \n `init(OFFER_STRING)`';
+var COPY_MSG = 'Great!, Now, copy, paste, and somehow give the ANSWER_STRING below to the receiver:';
+var MSG_HEADER = 'msg: ';
+var READY_MSG = 'Channel ready. To send a message "hi", type `say("hi")`';
+
+var SIGNALING_INFO = '\n The OFFER_STRING from the sender has info about its ip, which you have now recorded.';
+var SIGNALING_INFO_MORE = '\n You have generated an "answer", which has info including your ip address. By giving it to the sender by some means of your choice, you are playing the role of a "Signaling Server," which sets up connections.\n';
+
+console.log(WELCOME_MSG);
+
+function init(offerString) {
   initReceiver();
   offerHandle(offerString);
 }
@@ -13,19 +23,22 @@ function initReceiver() {
     if (e.candidate !== null) return;
 
     var json = JSON.stringify(receiverConnection.localDescription);
-    console.log('ANSWER STRING:'),
-    console.log(JSON.stringify(json)); // double stringify easier for console log escaping quotes
+    console.log(COPY_MSG),
+    console.log(JSON.stringify(json)); // twice stringified for escaping quote convenience on console log
+
+    console.info(SIGNALING_INFO);
+    console.info(SIGNALING_INFO_MORE);
   };
 
   receiverConnection.ondatachannel = function(e) {
     receiveChannel = e.channel;
 
     receiveChannel.onmessage = function(e) {
-      console.log('msg: ', e.data);
+      console.log(MSG_HEADER, e.data);
     };
 
     receiveChannel.onopen = function() {
-      console.log('receive channel open');
+      console.log(READY_MSG);
     }
   }
 }
--- sender.js
+++ sender.js
@@ -1,45 +1,58 @@
-var localConnection;
+var senderConnection;
 var sendChannel;
 
-function createConnection() {
+var WELCOME_MSG = 'Welcome! Type `init()` to generate an offer string!';
+var COPY_MSG = 'Copy, paste, and somehow give the OFFER_STRING below to the receiver:';
+var GET_ANSWER_MSG = '\n\n When you get an ANSWER_STRING from the receiver, copy and paste it wrapped in connect(), like this \n `connect(ANSWER_STRING)`';
+var READY_MSG = 'Channel ready. To send a message "hi", type `say("hi")`';
+var MSG_HEADER = 'msg:'
+
+var SIGNALING_INFO = '\n You have generated an "offer", which has info including your ip address. By giving it to the receiver by some means of your choice, you are playing the role of a "Signaling Server," which sets up connections.';
+var SIGNALING_INFO_MORE = '\n The receiver will record the info about your ip, and send you some info about how to contact it. \n ';
+
+console.log(WELCOME_MSG);
+
+function init() {
   var servers = null;
   var pcConstraint = null;
   var dataConstraint = null;
 
-  localConnection = new RTCPeerConnection(servers, pcConstraint);
+  senderConnection = new RTCPeerConnection(servers, pcConstraint);
 
-  sendChannel = localConnection.createDataChannel('sendDataChannel', dataConstraint);
+  sendChannel = senderConnection.createDataChannel('sendDataChannel', dataConstraint);
  
   sendChannel.onmessage = function(e) {
-    console.log('msg:', e.data);
+    console.log(MSG_HEADER, e.data);
   };
 
   sendChannel.onopen = function() {
-    console.log('send channel open');
+    console.log(READY_MSG);
   };
 
-  localConnection.createOffer().then(
+  senderConnection.createOffer().then(
     function(desc) {
-      console.log(desc);
-      localConnection.setLocalDescription(desc, function(){}, function(){});
+      senderConnection.setLocalDescription(desc, function(){}, function(){});
     },
     offerError
   );
 
-  localConnection.onicecandidate = function(e) {
-    console.log('onicecandidate');
-    console.log('event', e);
+  senderConnection.onicecandidate = function(e) {
     if (e.candidate !== null) return;
 
-    var json = JSON.stringify(localConnection.localDescription);
-    console.log('OFFER STRING:');
-    console.log(JSON.stringify(json)); // twice stringified for excaping quote convenience on console log
+    var json = JSON.stringify(senderConnection.localDescription);
+    console.log(COPY_MSG);
+    console.log(JSON.stringify(json)); // twice stringified for escaping quote convenience on console log
+
+    console.info(SIGNALING_INFO);
+    console.info(SIGNALING_INFO_MORE);
+
+    console.log(GET_ANSWER_MSG);
   };
 }
 
-function answerHandle(answerString) {
+function connect(answerString) {
   var answer = JSON.parse(answerString);
-  localConnection.setRemoteDescription(answer);
+  senderConnection.setRemoteDescription(answer);
 }
 
 // msg: String
